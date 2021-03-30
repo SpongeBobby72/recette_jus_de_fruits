7e7e3c90fd3807a5e07191b7b7b27190a60670ec
Pauline Triger <pauline.triger@gmail.com>
2021-03-30 16:50:32 +0200

    création contenu

--- themes/AdminLTE-3.1.0/suppr-recette.php
+++ themes/AdminLTE-3.1.0/suppr-recette.php
@@ -1,60 +1,69 @@
 <?php
 
-require ('../../back/config-sample.php');
+require('../../back/config-sample.php');
 
 try {
-    $db = new PDO($dsn, $username, $password);
+  $db = new PDO($dsn, $username, $password);
 } catch (PDOException $e) {
-    var_dump($e);
+  var_dump($e);
 }
 
-$recettes = $db->prepare('SELECT * FROM `recettes`, `fruits`
-WHERE recettes.ingredient_id = fruits.id ;');
-$recettes->execute();
 
-$toutesLesRecettes = [];
 
-while ($recette = $recettes->fetch()) {
-    foreach ($recette as $k => $v) {
-        if (is_int($k) == true) {
-            unset ($recette[$k]);
-        }
+$nomRecettes = $db->prepare('SELECT recettes.nom as recette, recettes.ingredient_id as ingrId, recettes.img img, ingredients.nom as ingrNom FROM `recettes`
+    LEFT JOIN ingredients ON recettes.ingredient_id = ingredients.id
+    ');
+$nomRecettes->execute();
+
+
+$recettes = [];
+
+while ($recette = $nomRecettes->fetch()) {
+  foreach ($recette as $k => $v) {
+    if (is_int($k) == true) {
+      unset ($recette[$k]);
     }
-    $toutesLesRecettes[] = $recette;
+  }
+  $recettes[] = $recette;
 }
 
-var_dump($toutesLesRecettes);
+var_dump($recettes);
+
 
 ?>
 
 <!doctype html>
 <html lang="fr">
 <head>
-    <meta charset="UTF-8">
-    <meta name="viewport"
-          content="width=device-width, user-scalable=no, initial-scale=1.0, maximum-scale=1.0, minimum-scale=1.0">
-    <meta http-equiv="X-UA-Compatible" content="ie=edge">
-    <title>Liste des recettes</title>
+  <meta charset="UTF-8">
+  <meta name="viewport"
+        content="width=device-width, user-scalable=no, initial-scale=1.0, maximum-scale=1.0, minimum-scale=1.0">
+  <meta http-equiv="X-UA-Compatible" content="ie=edge">
+  <title>Liste des recettes</title>
 </head>
 <body>
 
 <h1>Liste des recettes</h1>
 
+<?php
+  foreach ($toutesLesRecettes as $recette) {
+?>
 <div id="recettes">
-    <div id="recette">
-        <h2><?php echo $recette['nom'] ?></h2>
-        <div>
-            <h3>Ingrédients :</h3>
-            <ul>
-                <li><?php echo $recette['nom']?></li>
-                <li><?php echo $recette['nom']?></li>
+  <div id="recette">
+    <h2><?php echo $recette['recette'] ?></h2>
+    <div><img src="../../assets-jus/<?php echo $recette['img'] ?>" alt="<?php echo $recette['recette'] ?>"></div>
+    <div>
+      <h3>Ingrédients :</h3>
+      <ul>
+        <li><?php echo $recette['ingrNom'] ?></li>
 
-            </ul>
-        </div>
-        <div><img src="<?php $recette['nom'] ?>" alt="<?php echo $recette['nom'] ?>"></div>
+      </ul>
     </div>
+  </div>
 
-
+  <?php
+  }
+  ?>
 
 </body>
 </html>
