d7293f465003a6886a43a2db781c70c1a28f61d4
Pauline Triger <pauline.triger@gmail.com>
2021-03-31 09:11:36 +0200

    Ajout du style sur la page

--- themes/AdminLTE-3.1.0/suppr-recette.php
+++ themes/AdminLTE-3.1.0/suppr-recette.php
@@ -36,18 +36,9 @@ if ( isset($_GET['delete_id']) ) {
   header('Location:suppr-recette.php');
 }
 
-?>
+include 'header.php';
 
-<!doctype html>
-<html lang="fr">
-<head>
-  <meta charset="UTF-8">
-  <meta name="viewport"
-        content="width=device-width, user-scalable=no, initial-scale=1.0, maximum-scale=1.0, minimum-scale=1.0">
-  <meta http-equiv="X-UA-Compatible" content="ie=edge">
-  <title>Liste des recettes</title>
-</head>
-<body>
+?>
 
 <h1>Liste des recettes</h1>
 
@@ -56,14 +47,11 @@ if ( isset($_GET['delete_id']) ) {
 ?>
 <div id="recettes">
   <div id="recette">
-    <h2><?php echo $recette['nom'] . ' <a href="/themes/AdminLTE-3.1.0/suppr-recette.php?delete_id='.$recette['id'].'">Supprimer la recette</a>';
-
-      ?></h2>
+    <h2><?php echo $recette['nom'] ?> </h2><br>
+ <?php echo '<p><a href="/themes/AdminLTE-3.1.0/suppr-recette.php?delete_id='.$recette['id'].'">Supprimer la recette</a></p>' ?>
   </div>
 
   <?php
   }
-  ?>
-
-</body>
-</html>
+include 'footer.php';
+?>
